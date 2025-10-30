d3aff986072527606789a26202e6c31a9277b541
ClaudiaHug <claudia.huguet@dsm-firmenich.com>
2025-10-30 10:22:57 +0100

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 
 const App: React.FC = () => {
-    return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        flexDirection: 'column',
+      }}
+    >
+      <h1>Hello World</h1>
+      <button
+        style={{
+          padding: '10px 20px',
+          fontSize: '16px',
+          cursor: 'pointer',
+          marginTop: '20px',
+        }}
+        onClick={() => alert('¡Botón clickeado!')}
+      >
+        Click here
+      </button>
+    </div>
+  );
 };
 
 export default App;
